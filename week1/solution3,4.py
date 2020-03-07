4a0dc05fe9369c11f586ac8cb4f8b91867e8285e
Argentum462 <zavodova121@mail.ru>
2020-03-07 20:55:04 +0500

    New solution

---
+++ week1/solution3,4.py
@@ -0,0 +1,6 @@
+human = int(input())
+apple = int(input())
+# solution 3
+print(apple // human)
+# solution 4
+print(apple % human)
---
+++ week1/solution6-8.py
@@ -0,0 +1,7 @@
+number = int(input())
+# solution 6
+print(number % 10)
+# solution 7
+print(number // 10)
+# solution 8
+print(number // 10 % 10)
