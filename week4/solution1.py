0f9d2c856c5bb4a4527b7aa47eb8d315a2847712
Argentum462 <zavodova121@mail.ru>
2020-03-15 19:37:27 +0500

    New solution in week4

---
+++ week4/solution1.py
@@ -0,0 +1,7 @@
+# Напишите функцию min4(a, b, c, d), вычисляющую минимум четырех чисел
+def min4(a, b, c, d):
+    return min(a, b, c, d)
+
+a, b = int(input()), int(input())
+c, d = int(input()), int(input())
+print(min4(a, b, c, d))
